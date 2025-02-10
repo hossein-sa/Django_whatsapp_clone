3d3bad53f02e04dfd359e069f695d00826bca934
Hossein Sadeghi <sadeghi.ho@hotmail.com>
2025-02-10 15:52:07 +0330

    Added Django REST Framework

--- whatsapp_clone/settings.py
+++ whatsapp_clone/settings.py
@@ -54,6 +54,17 @@ INSTALLED_APPS = [
     'chat_messages',
 ]
 
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+}
+
 AUTH_USER_MODEL = 'users.User'
 
 
