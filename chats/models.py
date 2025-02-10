6131eaed13d74af01f4c6858ac9edd4f3dbbb8ab
Hossein Sadeghi <sadeghi.ho@hotmail.com>
2025-02-10 14:45:33 +0330

    feat: Add Chat model

--- chats/models.py
+++ chats/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from users.models import User
+import uuid
 
-# Create your models here.
+class Chat(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    sender = models.ForeignKey(User, related_name="chats_as_sender", on_delete=models.CASCADE)
+    recipient = models.ForeignKey(User, related_name="chats_as_recipient", on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Chat between {self.sender.email} and {self.recipient.email}"
