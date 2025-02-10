32f118c3e3f736b9bd0eed142785ff359801114f
Hossein Sadeghi <sadeghi.ho@hotmail.com>
2025-02-10 16:02:38 +0330

    Added User API Endpoints

--- users/views.py
+++ users/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
+from django.contrib.auth import get_user_model
+from .serializers import UserSerializer
 
-# Create your views here.
+User = get_user_model()
+
+class UserListView(generics.ListAPIView):
+    """List all users"""
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]  # Allow public access
